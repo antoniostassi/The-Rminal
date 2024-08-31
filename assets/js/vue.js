ff5556549f89ca94a0fde1f65bc14248663f87c6
Antonio Stassi <stassiant@outlook.it>
2024-08-31 18:02:53 +0200

    Terminal graphic improvement + vue.js interactions

--- assets/js/vue.js
+++ assets/js/vue.js
@@ -5,12 +5,28 @@ const { createApp } = Vue;
 const appCreation = createApp({
     data() {
         return {
-        message: 'Hello Vue!',
-        elements: []
+        historyChat: [
+            ""
+        ],
+        textInput: "",
+        character: {
+            userName: "antoniostassi",
+            machineName: "TheRminal"
+        }
         }
     },
     methods: {
-
+        tryCmd() {
+            if( this.textInput == 'clear' ) { // Se il comando eseguito è "clear"
+                this.historyChat = ""; // Pulisci la chat
+            } else {
+                this.sendCmd(this.textInput);
+            }
+            this.textInput = "";
+        },
+        sendCmd(input) {
+            this.historyChat.push(input);
+        }
     }
-}).mount('#app')
+}).mount('#TheRminal')
 

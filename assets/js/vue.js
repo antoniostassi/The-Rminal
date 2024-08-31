a669d44ed017eb0db8bf966b7c3655733a939584
Antonio Stassi <stassiant@outlook.it>
2024-08-31 18:24:59 +0200

    idling system + serious style fix

--- assets/js/vue.js
+++ assets/js/vue.js
@@ -12,7 +12,8 @@ const appCreation = createApp({
         character: {
             userName: "antoniostassi",
             machineName: "TheRminal"
-        }
+        },
+        idling: true 
         }
     },
     methods: {

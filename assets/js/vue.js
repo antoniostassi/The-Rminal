650fe42260d373d9c5ad4d7844cbd46433bd1c88
Antonio Stassi <stassiant@outlook.it>
2024-09-01 17:17:39 +0200

    Huge optimizations on string check + cd system.

--- assets/js/vue.js
+++ assets/js/vue.js
@@ -6,7 +6,7 @@ const appCreation = createApp({
     data() {
         return {
         character: {
-            lastLogin: "Sat Aug 31 12:31:09",
+            lastLogin: "",
             userName: "maliktm",
             machineName: "apple-m3"
         },
@@ -31,16 +31,49 @@ const appCreation = createApp({
                 trigger:"github",
                 function:"printSomething('JS Developer: https://github.com/antoniostassi','Illustrator: https://github.com/margheritamottana')",
                 description:"Creators of The Rminal"
+            },
+            {
+                trigger:"ls",
+                function:"ls()",
+                description:"Print list of contents of the directory"
+            },
+            {
+                trigger:"cd",
+                function:"cd(this.textInput)",
+                description:"Navigate inside a folder/directory"
+            },
+            {
+                trigger:"dir",
+                function:"printSomething(this.currentDir);",
+                description:"Print your current directory"
             }
         ],
+        directoryContents: {
+            "C:/": {
+                contents:"Applications, Music, Downloads, Documents"
+            },
+            "C:/applications": {
+                contents: "Github-Desktop.exe, VisualStudioCode.exe, DonkeyKong.exe, newfolder"
+            },
+            "C:/applications/newfolder": {
+                contents: "Github-Desktop"
+            }
+        }
+        ,
+        currentDir: "C:/",
         commandFound: false
-        
         }
     },
     methods: {
         tryCmd() {
+            this.historyChat.push("---------------");
+            this.historyChat.push(this.terminal + " ~ % " + this.textInput);
+            this.historyChat.push("---------------");
+            
+            let firstWordCommand = this.textInput.match(/\w+/g)[0];
             this.commands.forEach(element => {
-                if (this.textInput.includes(element.trigger)) {
+                if(this.isMatch(element.trigger, firstWordCommand)) { // Way better 
+                //if (this.textInput.includes(element.trigger)) {
                     eval("this."+element.function);
                     this.commandFound = true;
                 }
@@ -55,7 +88,6 @@ const appCreation = createApp({
             this.terminal = "Last login: "+(this.character.lastLogin)+" "+this.character.userName+"@"+this.character.machineName;
         },
         printSomething(arg, arg2, arg3, arg4) {
-            this.historyChat.push("---------------");
             this.historyChat.push(arg);
             this.historyChat.push(arg2);
             this.historyChat.push(arg3);
@@ -66,10 +98,49 @@ const appCreation = createApp({
             console.log("Chat has been cleaned.");
         },
         help() {
-            this.historyChat.push("---------------");
             this.commands.forEach(element => {
                 this.historyChat.push(element.trigger + " | " + element.description);
             });
+        },
+        ls(){
+            this.historyChat.push(this.directoryContents[this.currentDir].contents);
+        },
+        cd(directory){
+            const newDir = directory.match(/\w+/g)[1]; // Selezione la prima parola subito dopo il "cd";
+            const actualDir = this.directoryContents[this.currentDir].contents.toLowerCase(); // Variabile contentente i contents della directory attuale
+            if (directory.trim() == "cd ..") {
+                const slashes = (this.currentDir.split("/").length - 1); //3
+
+                const previousDir = []; // Array per salvare ogni singola cartella del percorso
+                for(let i=0; i<slashes; i++){
+                    previousDir[i] = this.currentDir.trim().split('/')[i]; // Prendi ad uno ad uno le cartelle prima di ogni " / "
+                }
+                //console.table(previousDir);
+
+                this.currentDir = ""; // Reinizializza la directory
+                previousDir.forEach(value => { // Per ogni cartella delle cartelle della directory
+                    this.currentDir += value + "/"; // Inseriscile ad 1 ad 1 nella current Directory
+                }); // Fino a formare la directory giusta
+
+                //console.table(previousDir);
+                //console.log(this.currentDir);
+                if (this.currentDir.length > 3){ // Se il percorso non è C:/
+                    this.currentDir = this.currentDir.slice(0, -1); // Rimuove l'ultimo " / "
+                }
+                this.historyChat.push("Current Directory: "+ this.currentDir);
+            }
+            else if(this.isMatch(actualDir, newDir.toLowerCase())) {
+                this.currentDir += "/"+newDir;
+                this.currentDir = this.currentDir.replace("C://", "C:/");
+                this.historyChat.push("Current Directory: "+ this.currentDir);
+            } else {
+                this.historyChat.push("Directory: "+ this.currentDir + newDir + " does not exists!");
+                this.textInput = '';
+            };
+        },
+        isMatch(searchOnString, searchText) { // Funzione per checkare l'esistenza dell'intera parola all'interno di una stringa
+            searchText = searchText.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+            return searchOnString.match(new RegExp("\\b"+searchText+"\\b", "i")) != null;
         }
     },
     mounted() {
